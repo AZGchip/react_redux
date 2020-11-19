edb723eb29f0bb7e248be9d35c3194c846633713
AZGchip <andrew@zinski.net>
2020-11-18 22:05:58 -0500

    added state to searchbar.js. added handlers for search filter changes

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
 
+
 class Searchbar extends Component {
-    constructor(props) {
-        super(props)
 
+    //default search filter is "story"
+    state = {
+        searchtype: "story",
+        searchby: "relevance"
+    };
 
-    }
+    // when search filter is changed
+    handleSearchChange = (event) => {
+        this.setState({ searchtype: event.target.value });
+    };
+    // when searchby selector is changed
+    handleSearchByChange = (event) => {
+        this.setState({ searchby: event.target.value });
+    };
+
+    // when search button is pressed
+    buildAndCall = (event) => {
+        // stops reload
+        event.preventDefault();
+        const defaultApi = "http://hn.algolia.com/api/v1/search";
+
+    };
+
     render() {
         return (
             <div>
+                {/* title */}
                 <h2>Hacker News Searcher</h2>
                 <form>
-                    <input type="text"/>
-                    <button onClick={this.callApi}>Search</button>
-                    <br/>
-                    <input type="radio" name="searchfilter" value="story" id="story" onChange={this.handleSearchChange} checked />
+                    {/* Radio buttons */}
+                    <input type="radio" name="searchfilter" value="story" id="story" onChange={this.handleSearchChange} />
                     <label for="story">Story</label>
                     <br />
                     <input type="radio" name="searchfilter" value="poll" id="poll" onChange={this.handleSearchChange} />
-                    <label for="story">Poll</label>
+                    <label for="poll">Poll</label>
                     <br />
                     <input type="radio" name="searchfilter" value="author" id="author" onChange={this.handleSearchChange} />
-                    <label for="story">Author</label>
+                    <label for="author">Author</label>
+                    <br />
+                    {/* search box */}
+                    <input type="text" />
+                    {/* date/relevence selector */}
+                    <select onChange={this.handleSearchByChange}>
+                        <option value="relevance">Relevance</option>
+                        <option value="date">Date</option>
+                    </select>
+                    <button onClick={this.buildAndCall}>Search</button>
+
                 </form>
+                <p>search {this.state.searchtype} {this.state.searchby}</p>
             </div>
 
         )
