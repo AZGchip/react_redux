bec8df708945056934aee0f08dc358df0d7eee4c
AZGchip <andrew@zinski.net>
2020-11-22 19:41:02 -0500

    cleaned code. added comments. generated readme using my readme_generator.

--- README.md
+++ README.md
@@ -1,70 +1,32 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# react_redux
+          
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-no-red.svg)](https://GitHub.com/AZGchip/react_redux/graphs/commit-activity)
+[![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
+      
+      
+In this project, I was tasked with creating a website that searches Hacker new's api and storing search data using redux.
 
-## Available Scripts
+## Table Of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contact](#contact)
 
-In the project directory, you can run:
+      
+## <a name ="Installation"></a> Installation
+Replace the Src and Public Folder in a new react application with the ones from this repo. npm install both "react-redux" and "redux". Then run npm start
 
-### `npm start`
+      
+## <a name ="Usage"></a> Usage
+Users can search Hacker News by relevancy or post date and filter searches by post, poll, or author.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+      
+## <a name ="contact"></a> Contact me
+AZGchip  
+[![GitHub followers](https://img.shields.io/github/followers/AZGchip.svg?style=social&label=Follow&maxAge=2592000)](https://github.com/AZGchip?tab=followers)
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
-### `npm test`
+<img src="https://avatars2.githubusercontent.com/u/60306563?v=4" alt="profile" width="100"/>
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+If you have any questions, please contact me at:  [github.com/AZGchip](https://github.com/AZGchip). 
+Created by Andrew Zinski (Github:AZGchip)
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,11 +5,11 @@ import { connect } from "react-redux"
 class Searchbar extends Component {
     constructor(props) {
         super(props)
-        // searchby is default query
+        // searchby is default by query
+        // local state used for search filters
         this.state = {
             searchtype: "",
             searchby: "?query=",
-            text: "",
             searchResults: []
         };
     }
@@ -18,38 +18,41 @@ class Searchbar extends Component {
 
     // when search filter is changed
     handleSearchChange = (event) => {
+        //update local state for tags
         this.setState({ searchtype: event.target.value });
     };
+
     // when searchby selector is changed
     handleSearchByChange = (event) => {
+        //update local state for searchby
         this.setState({ searchby: event.target.value });
     };
+
     //when text is entered
     handleTextChange = (event) => {
-        this.props.onChangeName(event.target.value)
+        //update redux state for text
+        this.props.onChangeText(event.target.value);
+
     }
 
-    handleNameChange = (event) => {
-        
-      }
+
     // when search button is pressed
     buildAndCall = (event) => {
-
-        // store.dispatch(saveSearch())
-        // stops reload from form submit
+        // stops default reload on form submit
         event.preventDefault();
-        //update redux through 
-        this.props.onAddName()
+        //update redux state for searched text
+        this.props.onAddSearch()
+
         //basic api call 
         const defaultCall = "http://hn.algolia.com/api/v1/search"
-        // assigns variables using the state of the search bar
+        // assigns variables using the state of the search bar and redux state
         let text = this.props.text;
         let tag = this.state.searchtype;
         let searchBy = this.state.searchby;
         let query;
         //makes sure searchbar isnt empty before searching 
         if (text !== "" || text.replace(/\s/g, "") !== "") {
-            console.log(tag)
+            // builds api query based on local state 
             if (tag === "&tags=author_") {
                 query = defaultCall + searchBy + tag + text
             }
@@ -65,16 +68,15 @@ class Searchbar extends Component {
                         //returned data is formated and saved to state
                         let results = []
                         results = result.hits.map(data =>
-                            <div className="col-md-3 m-2 border-dark">
+                            <div key={data.title} className="col-md-3 m-2 border-dark">
                                 <h3>{data.title}</h3>
                                 <h4>by {data.author}</h4>
                                 <p>{data.created_at}</p>
                             </div>)
-                        console.log(results)
                         this.setState({ searchResults: results })
 
                     },
-
+                    //if err
                     (error) => {
                         this.setState({
                             error
@@ -84,43 +86,61 @@ class Searchbar extends Component {
 
 
         }
-        else { this.setState({ searchResults: <div className="text-danger">please fill in search box</div> }) }
+        // if text box is empty, tell user to fill in box before searching 
+        else { this.setState({ searchResults: <div className="text-danger row">please fill in search box</div> }) }
     };
 
     render() {
 
         return (
-            <div className="container">
-                {/* title */}
-                <h2 className="row text-center">Hacker News Searcher</h2><hr/>
-                <form className="col-md-8">
-                    {/* Radio buttons */}
-                    <input type="radio" name="searchfilter" value="&tags=story" id="story" onChange={this.handleSearchChange} />
-                    <label htmlFor="story">Story</label>
-                    <br />
-                    <input type="radio" name="searchfilter" value="&tags=poll" id="poll" onChange={this.handleSearchChange} />
-                    <label htmlFor="poll">Poll</label>
-                    <br />
-                    <input type="radio" name="searchfilter" value="&tags=author_" id="author" onChange={this.handleSearchChange} />
-                    <label htmlFor="author">Author</label>
-                    <br />
-                    {/* search box */}
-                    <input type="text" onChange={this.handleTextChange} />
-                    {/* date/relevence selector */}
-                    <select onChange={this.handleSearchByChange}>
-                        <option value="?query=">Relevance</option>
-                        <option value="_by_date?=">Date</option>
-                    </select>
-                    <button onClick={this.buildAndCall}>Search</button>
+            // page contents
+            <div className="row">
 
-                </form>
-                    <div className="col-md-4">
+                {/* title */}
+                <h2 className="row text-center">Hacker News Searcher</h2>
+                <hr />
+
+                <div className="row">
+                    {/* Search form */}
+                    <form className="col-md-8">
+
+                        {/* Radio buttons for search tag selection*/}
+                        <input type="radio" name="searchfilter" value="&tags=story" id="story" onChange={this.handleSearchChange} />
+                        <label htmlFor="story">Story</label>
+                        <br />
+                        <input type="radio" name="searchfilter" value="&tags=poll" id="poll" onChange={this.handleSearchChange} />
+                        <label htmlFor="poll">Poll</label>
+                        <br />
+                        <input type="radio" name="searchfilter" value="&tags=author_" id="author" onChange={this.handleSearchChange} />
+                        <label htmlFor="author">Author</label>
+                        <br />
+
+                        {/* search text box */}
+                        <input type="text" onChange={this.handleTextChange} value={this.props.text} />
+
+                        {/* date/relevence selector */}
+                        <select onChange={this.handleSearchByChange}>
+                            <option value="?query=">Relevance</option>
+                            <option value="_by_date?=">Date</option>
+                        </select>
+
+                        {/* Submit search button */}
+                        <button onClick={this.buildAndCall}>Search</button>
+
+                    </form>
+
+                    {/* Saved redux state box */}
+
+                    <div className="col-md-4 ">
                         <h4>Previous Searches</h4>
-                        {this.props.searches && this.props.searches.map(text => (
-                            <div className="" key={text}> {text}</div>
-                        ))}
+                        <div className="scroll">
+                            {this.props.searches && this.props.searches.map(text => (
+                                <div value={text} key={text}> {text}</div>
+                            ))}
+                        </div>
                     </div>
-                {/* results box */}
+                </div>
+                {/* api results box */}
                 <div className="row ">
                     {this.state.searchResults}
                 </div>
@@ -140,8 +160,8 @@ const mapStatetoProps = (state) => {
 //allows the component to manipulate the redux state
 const mapDispatchtoProps = (dispatch) => {
     return {
-        onChangeName: (text) => dispatch({ type: "CHANGE_TEXT", text: text }),
-        onAddName: () => dispatch({ type: "ADD_SEARCH" }),
+        onChangeText: (text) => dispatch({ type: "CHANGE_TEXT", text: text }),
+        onAddSearch: () => dispatch({ type: "ADD_SEARCH" }),
     }
 }
 
